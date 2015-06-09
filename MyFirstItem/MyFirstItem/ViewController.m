7ae52360c20e07d77ea3da5ca32127ed2794d6c2
李长青 <changqingli_vip@163.com>
2015-06-09 22:39:22 +0800

    初始化项目.

---
+++ MyFirstItem/MyFirstItem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyFirstItem
+//
+//  Created by 李长青 on 15/6/9.
+//  Copyright (c) 2015年 李长青. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
